1d792efa260640c24cce5db7e95a66becf1b9e9a
schadauer <andreas@simpledesign.io>
2019-03-25 11:41:23 +0100

    Create FormSteps component

---
+++ src/components/Form/FormSteps/FormSteps.js
@@ -0,0 +1,21 @@
+/*
+*
+* FormSteps create the steps for the tansaction forms. It also contains the progress bar....
+*
+* 
+*/
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+function FormSteps(props) {
+
+    const FormSteps = styled.div`
+    
+    `;
+
+    return <FormField />
+}
+
+export default FormSteps;
---
+++ src/components/Form/FormSteps/index.js
@@ -0,0 +1,1 @@
+import FormSteps from './FormSteps'
