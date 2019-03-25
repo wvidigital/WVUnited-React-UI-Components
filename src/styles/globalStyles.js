f25d8ed3c8b70244492a863f090817ebb97653f8
schadauer <andreas@simpledesign.io>
2019-03-25 11:36:14 +0100

    Add global styles to styles folder

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,10 @@
+const globalStyles = {
+    colors: {
+        textColor: '#363636',
+        errorTextColor: '#e54f5e',
+        formBackground: '#f1f1f1'
+    },
+    fontSize: '16px'
+}
+
+export default globalStyles;
