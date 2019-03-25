91bfaa819de197edc20eae512addcc45cd3f2115
schadauer <andreas@simpledesign.io>
2019-03-25 11:50:30 +0100

    Change path to FieldSelect

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 // Export all UI components for the library.
 export { default as Text } from './Text';
 export { default as Heading } from './Heading';
-export { default as FieldSelect } from './FieldSelect';
+export { default as FieldSelect } from './Form/FieldSelect';
 export { default as TextLink } from './Button/TextLink';
 export { default as LinkButton } from './Button/LinkButton';
 export { default as ActionButton} from './Button/ActionButton';
