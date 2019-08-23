aa7f4ef7a1338b27fc95722b943ff4d83f09b96a
Glenn Reyes <glenn@glennreyes.com>
2019-08-23 15:17:55 +0200

    Remove log

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Card as RawCard } from 'rebass/styled-components';
 
-const Card = props =>
-  console.log(props) || (
-    <RawCard bg="white" sx={{ borderRadius: 1 }} {...props} />
-  );
+const Card = props => (
+  <RawCard bg="white" sx={{ borderRadius: 1 }} {...props} />
+);
 
 export default Card;
